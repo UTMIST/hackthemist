87eab818a06f3c938dd4e607ed0bbd1e586b0595
Henry Tu <henry@henrytu.me>
2022-11-07 23:33:49 -0500

    Added mailinglist + polished navbar (#3)
    * Mailing list enrollment
    
    * Transparent navbar when docked
    
    * Dismiss navbar when clicking item on mobile
    
    * lol

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
-import {Container, Stack, Box, Heading, Button, Text} from '@chakra-ui/react'
+import {Container, Stack, Box, Heading, Button, Text, Icon} from '@chakra-ui/react'
 import {scrollToSection} from "../util/scroll";
 import audience from '../img/audience.jpg';
+import {
+    EmailIcon,
+} from '@chakra-ui/icons';
 
 const Component = () => (
     <Box style={{width: '100vw', backgroundImage: `url(${audience})`, backgroundSize: 'cover', backgroundAttachment: 'fixed'}}>
-        <Box background={'rgba(0, 0, 0, 0.5)'}>
+        <Box background={'rgba(0, 0, 0, 0.7)'}>
             <Container maxW={'3xl'}>
                 <Stack
                     as={Box}
                     textAlign={'center'}
                     spacing={{base: 8, md: 14}}
-                    py={{base: 20, md: 36}}>
+                    py={{base: 20, md: 36}}
+                    style={{paddingTop: '200px', paddingBottom: '200px'}}>
                     <Heading
                         color='white'
                         fontWeight={600}
-                        fontSize={{base: '5xl', sm: '5xl', md: '6xl'}}>
+                        fontSize={{base: '5xl', sm: '5xl', md: '7xl'}}>
                         Hack the MIST
                     </Heading>
                     <Heading
@@ -25,17 +29,37 @@ const Component = () => (
                         Spring 2022 @ University of Toronto | Presented by UTMIST
                     </Heading>
                     <Text color={'gray.200'}>
-                        "we really like niagara falls, so that's why we started this hackathon"
+                        "we clear the mist around machine learning 🥵💦🥵💦"
                     </Text>
-                    <Text color={'gray.200'}>
-                        (embed mailchimp integration here)
+                    <Text color={'white'}>
+                        Join our mailing list to be notified when our applications open!
                     </Text>
                     <Stack
-                        direction={'column'}
+                        style={{marginTop: "20px"}}
+                        direction={{base: 'column', sm: 'column', md: 'row'}}
                         spacing={3}
                         align={'center'}
                         alignSelf={'center'}
                         position={'relative'}>
+                        <Button
+                            colorScheme={'blue'}
+                            bg={'blue.600'}
+                            rounded={'full'}
+                            px={6}
+                            _hover={{
+                                bg: 'blue.500',
+                            }}
+                            onClick={() => window.open('https://eepurl.com/dGMddD', '_blank')}>
+
+                            <Icon
+                                as={EmailIcon}
+                                w={6}
+                                h={6}
+                                style={{marginRight: '10px'}}
+                            />
+
+                            Join our mailing list
+                        </Button>
                         <Button
                             colorScheme={'blue'}
                             bg={'blue.600'}
