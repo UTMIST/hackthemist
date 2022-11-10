cb145cdcbf81ac72a0f27fbd585cb67641c22d42
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-11-09 20:14:17 -0500

    added social media icons to contact us section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -6,17 +6,54 @@ import {
     Stack,
     Text,
     Button,
-    Icon
+    Icon,
+    Link,
+    Center
 } from '@chakra-ui/react';
 import { EmailIcon } from '@chakra-ui/icons';
+import {
+    FaEnvelope,
+    FaFacebook,
+    FaInstagram,
+    FaLinkedin,
+    FaTwitter
+} from 'react-icons/fa'
 
 const Contact = () => {
+    const socialData = [
+        {
+            aria: 'Email',
+            icon: <FaEnvelope />,
+            href: 'mailto:utorontomist@gmail.com'
+        },
+        {
+            aria: 'Facebook',
+            icon: <FaFacebook />,
+            href: 'https://www.facebook.com/UofT.MIST'
+        },
+        {
+            aria: 'Instagram',
+            icon: <FaInstagram />,
+            href: 'https://instagram.com/uoft_utmist'
+        },
+        {
+            aria: 'LinkedIn',
+            icon: <FaLinkedin />,
+            href: 'https://linkedin.com/company/utmist'
+        },
+        {
+            aria: 'Twitter',
+            icon: <FaTwitter />,
+            href: 'https://twitter.com/UTMIST1'
+        }
+    ]
+
     return (
-        <Container maxW={'3xl'}>
+        <Container id="contact-us" maxW={'3xl'}>
             <Stack pb={20} gap={4}>
                 <Heading>Contact Us</Heading>
-                <Text as={'b'}>Feel free to reach out through email or visit our social media!</Text>
-                <Flex>
+                <Text as={'b'}>Feel free to reach out via email or visit our social media!</Text>
+                <Flex gap={6}>
                     <Button
                         colorScheme={'blue'}
                         bg={'blue.600'}
@@ -33,9 +70,21 @@ const Contact = () => {
                             h={6}
                             style={{ marginRight: '10px' }}
                         />
-
                         Join our mailing list
                     </Button>
+                    {
+                        socialData.map(social => (
+                            <Center>
+                                <Link
+                                    aria-label={social.aria}
+                                    href={social.href}
+                                    fontSize={20}
+                                    isExternal>
+                                    {social.icon}
+                                </Link>
+                            </Center>
+                        ))
+                    }
                 </Flex>
             </Stack>
         </Container>
