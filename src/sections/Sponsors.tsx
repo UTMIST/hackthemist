9dd4265336dac73b2ea4bcbfec3a32ab87b94e2d
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2022-11-11 21:02:00 -0500

    Fixed email link

--- src/sections/Sponsors.tsx
+++ src/sections/Sponsors.tsx
@@ -14,7 +14,7 @@ const Sponsors = () => {
             <Stack pb={20} gap={4}>
                 <Heading>Sponsors</Heading>
                 <Text>Interested in sponsoring us? Get in touch with our team at&nbsp;
-                    <Link textDecor={'underline'} href="utmist.outreach@gmail.com">
+                    <Link textDecor={'underline'} href="mailto:utmist.outreach@gmail.com">
                         utmist.outreach@gmail.com
                     </Link>
                 </Text>
