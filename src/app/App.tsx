a653bcd72cd34818f4c9d3de48a44578f45f5757
Harshith <harshithl1777@gmail.com>
2023-01-04 14:47:17 -0500

    fix: raleway font is not being used

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,27 @@
 import * as React from 'react';
-import {ChakraProvider} from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../config/theme';
 import Header from '../sections/Header';
 import AboutUs from '../sections/AboutUs';
 import Navigation from '../sections/Navigation';
-import "../style/override.css";
 import Footer from '../sections/Footer';
 import Contact from '../sections/Contact';
 import Sponsors from '../sections/Sponsors';
 import FAQ from '../sections/FAQ';
+import '../style/override.css';
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
 
 const App = () => {
     return (
         <ChakraProvider theme={theme}>
-            <Navigation/>
-            <Header/>
-            <AboutUs/>
-            <Sponsors/>
-            <FAQ/>
-            <Contact/>
-            <Footer/>
+            <Navigation />
+            <Header />
+            <AboutUs />
+            <Sponsors />
+            <FAQ />
+            <Contact />
+            <Footer />
         </ChakraProvider>
     );
 };
