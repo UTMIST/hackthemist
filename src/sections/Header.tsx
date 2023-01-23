80c8d14f6c1408c38b3b11c5bda12b5f9bc52665
Henry Tu <henry@henrytu.me>
2023-01-22 19:25:43 -0500

    Added buttons to application page

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import {Container, Stack, Box, Heading, Button, Text, Icon, Image, Center} from '@chakra-ui/react'
-import {scrollToSection} from "../util/scroll";
+import {Container, Stack, Box, Heading, Button, Image, Center} from '@chakra-ui/react'
 import Hacker from "../components/Hacker";
 import headerLogo from '../img/header_logo.svg';
-import {
-    EmailIcon,
-} from '@chakra-ui/icons';
 
 const Component = () => (
     <Container maxW={'3xl'}>
@@ -28,11 +24,8 @@ const Component = () => (
             <Center>
                 <Image src={headerLogo} className="headerlogo"/>
             </Center>
-            <Text color={'white'}>
-                Join our mailing list to be notified when our applications open!
-            </Text>
             <Stack
-                style={{marginTop: "20px"}}
+                style={{marginTop: "40px"}}
                 direction={{base: 'column', sm: 'column', md: 'row'}}
                 spacing={3}
                 align={'center'}
@@ -49,16 +42,8 @@ const Component = () => (
                     _active={{
                         bg: 'button_pressed',
                     }}
-                    onClick={() => window.open('https://eepurl.com/dGMddD', '_blank')}>
-
-                    <Icon
-                        as={EmailIcon}
-                        w={6}
-                        h={6}
-                        style={{marginRight: '10px'}}
-                    />
-
-                    Join our mailing list
+                    onClick={() => window.open('https://airtable.com/shrLynAHMZ1yA1tE9', '_blank')}>
+                    Apply as Hacker
                 </Button>
                 <Button
                     colorScheme={'blue'}
@@ -71,8 +56,8 @@ const Component = () => (
                     _active={{
                         bg: 'button_pressed',
                     }}
-                    onClick={() => scrollToSection('#about-us')}>
-                    Learn More
+                    onClick={() => window.open('https://airtable.com/shrwePl8daOVkdv1p', '_blank')}>
+                    Apply as Mentor
                 </Button>
             </Stack>
         </Stack>
